3fbf51eb989f5a3237ebe881fc0825e9b8844b7a
IsaacAlberson <isaacalberson24@gmail.com>
2023-12-03 09:08:56 -0800

    Instagram App

---
+++ app/src/main/java/com/example/isaacinstagramclone/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.isaacinstagramclone
+
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.content.FileProvider
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.isaacinstagramclone.fragments.ComposeFragment
+import com.example.isaacinstagramclone.fragments.FeedFragment
+import com.example.isaacinstagramclone.fragments.ProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.parse.FindCallback
+import com.parse.ParseException
+import com.parse.ParseFile
+import com.parse.ParseQuery
+import com.parse.ParseUser
+import java.io.File
+
+/**
+ * Let user create a post by taking a photo with their camera
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val fragmentManager: FragmentManager = supportFragmentManager
+
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnItemSelectedListener {
+            item ->
+
+            var fragmentToShow: Fragment? = null
+            when (item.itemId) {
+
+                R.id.action_home -> {
+                    fragmentToShow = FeedFragment()
+                }
+                R.id.action_compose -> {
+                    fragmentToShow = ComposeFragment()
+                }
+                R.id.action_profile -> {
+                    fragmentToShow = ProfileFragment()
+                }
+            }
+
+            if (fragmentToShow != null) {
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragmentToShow).commit()
+            }
+
+            true
+        }
+
+        // Set default selection
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).selectedItemId = R.id.action_home
+
+    }
+
+
+    companion object {
+        const val TAG = "MainActivity"
+    }
+}
